75bc63b5037ee4ec5c6d34f088d54e4c6c1349fb
levskobelev <levskobelev@gmail.com>
2025-03-19 12:11:41 +0400

    Resolved merge conflicts

--- conftest.py
+++ conftest.py
@@ -4,10 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 pytest_plugins = [
-<<<<<<< HEAD
+
     'fixtures.page',
     'fixtures.user_auth'
-=======
-    'fixtures.page'
->>>>>>> 1c55bef61e1ce6f79a8c226e41b430883257fc92
+
 ]
